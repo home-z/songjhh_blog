f00db42695d9aeceb3f78cb216443e7f54f0ac24
ryen <curiositygoddess@gmail.com>
2018-05-15 22:56:05 +0800

    change1

--- src/test/java/test/Test1WebPath.java
+++ src/test/java/test/Test1WebPath.java
@@ -1,16 +1,15 @@
 package test;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test1WebPath {
 
 	public static void main(String[] args) {
-		ApplicationContext app = new ClassPathXmlApplicationContext("springmvc.xml");
+		ApplicationContext app = new ClassPathXmlApplicationContext("classpath:springmvc.xml");
 		System.out.println(app.getBean("conversionService"));
+		System.out.println("dad");
 	}
 	
-	public void test() {
-		
-	}
 }
