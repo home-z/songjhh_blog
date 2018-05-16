4772e66a7a45cc9b5eb156a436f7483097d44ed0
ryen <curiositygoddess@gmail.com>
2018-05-16 22:10:24 +0800

    test

--- src/test/java/test/Test1WebPath.java
+++ src/test/java/test/Test1WebPath.java
@@ -7,9 +7,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test1WebPath {
 
 	public static void main(String[] args) {
-		ApplicationContext app = new ClassPathXmlApplicationContext("classpath:springmvc.xml");
+		ApplicationContext app = new ClassPathXmlApplicationContext("spring/pringmvc.xml");
 		System.out.println(app.getBean("conversionService"));
 		System.out.println("dad");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("sdfjal");
 	}
 	
 }
